eb81d6f5b6ac872a1480b3eaf5da485cd4e711a6
LHGRylynn <729935896@qq.com>
2021-01-12 12:11:18 +0800

    re

--- src/components/Home/Project/Require/Require.vue
+++ src/components/Home/Project/Require/Require.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    
-    <el-card class="reqt_card" >
-      <el-table :data="tableData" style="width: 100% ; height: 100%" @row-click="change" >
+    <el-button type="text" id="new_task">
+      <i class="el-icon-circle-plus-outline"></i>新建
+    </el-button>
+    <el-card class="reqt_card">
+      <el-table :data="reqtList" style="width: 100% ; height: 100%" @row-click="change">
         <el-table-column type="index" width="50"></el-table-column>
         <el-table-column
           prop="priority"
@@ -20,7 +22,7 @@
         <el-table-column prop="title" label="标题" width="180" align="center"></el-table-column>
         <el-table-column prop="kind" label="分类" width="180" align="center"></el-table-column>
         <el-table-column prop="type" label="类型" width="180" align="center"></el-table-column>
-        <el-table-column prop="sprintName" label="所属迭代" width="150" align="center"></el-table-column>
+        <el-table-column prop="stitle" label="所属迭代" width="150" align="center"></el-table-column>
         <el-table-column
           prop="endDate"
           label="截止日期"
@@ -30,11 +32,16 @@
           align="center"
           column-key="date"
         ></el-table-column>
+        <el-table-column prop="description" label="需求描述" width="200" align="center"></el-table-column>
       </el-table>
     </el-card>
 
     <el-card class="form_card" v-if="isForm">
-      <i class="el-icon-close" style="position:relative; left:740px; top:-10px;cursor:pointer"  @click="close"></i>
+      <i
+        class="el-icon-close"
+        style="position:relative; left:740px; top:-10px;cursor:pointer"
+        @click="close"
+      ></i>
       <el-form
         :model="ruleForm"
         :rules="rules"
@@ -54,7 +61,11 @@
             <el-option label="最低" value="最低"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="需求分类" prop="kind" style="float:right; margin-top:-70px;margin-right:20px">
+        <el-form-item
+          label="需求分类"
+          prop="kind"
+          style="float:right; margin-top:-70px;margin-right:20px"
+        >
           <el-select v-model="ruleForm.kind" placeholder="请选择需求分类">
             <el-option label="技术需求" value="技术需求"></el-option>
             <el-option label="功能需求" value="功能需求"></el-option>
@@ -73,16 +84,23 @@
         </el-form-item>
         <el-form-item label="截止时间" prop="endDate" style="width:600px" required>
           <el-col :span="11">
-              <el-date-picker
-                type="date"
-                placeholder="选择日期"
-                v-model="ruleForm.endDate"
-                style="width: 100%;"
-              ></el-date-picker>
+            <el-date-picker
+              type="date"
+              placeholder="选择日期"
+              v-model="ruleForm.endDate"
+              style="width: 100%;"
+            ></el-date-picker>
           </el-col>
         </el-form-item>
-        <el-form-item label="需求描述" >
-          <el-input type="textarea" v-model="ruleForm.description" ></el-input>
+        <el-form-item label="所属迭代" prop="sprint">
+          <el-select v-model="ruleForm.sprintName" placeholder="请选择所属迭代">
+            <el-option v-for="item in sprintList" :key="item.index">
+              <el-option :label="item.sprintName" :value="item.sprintName"></el-option>
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="需求描述">
+          <el-input type="textarea" v-model="ruleForm.description"></el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="submitForm('ruleForm')">立即更改</el-button>
@@ -108,112 +126,28 @@ export default {
   name: "ProjectSprint",
   data() {
     return {
+      pID: 1,
       pri: "success",
-      isForm:0,
-      tableData: [
+      isForm: 0,
+      sprintList: [
         {
-          title: "测试",
-          kind: "技术需求",
-          type: "新功能",
-          priority: "最高",
-          description: "这是一个测试",
-          endDate: "2021-01-13",
-          sprintName: "迭代1",
+          sprintName: "第一个迭代",
         },
         {
-          title: "测试",
-          kind: "技术需求",
-          type: "新功能",
-          priority: "较高",
-          description: "这是一个测试",
-          endDate: "2021-01-13",
-          sprintName: "迭代1",
-        },
-        {
-          title: "测试",
-          kind: "技术需求",
-          type: "新功能",
-          priority: "较高",
-          description: "这是一个测试",
-          endDate: "2021-01-13",
-          sprintName: "迭代1",
-        },
-        {
-          title: "测试",
-          kind: "技术需求",
-          type: "新功能",
-          priority: "较高",
-          description: "这是一个测试",
-          endDate: "2021-01-13",
-          sprintName: "迭代1",
-        },
-        {
-          title: "测试",
-          kind: "技术需求",
-          type: "新功能",
-          priority: "较高",
-          description: "这是一个测试",
-          endDate: "2021-01-13",
-          sprintName: "迭代1",
-        },
-        {
-          title: "测试",
-          kind: "技术需求",
-          type: "新功能",
-          priority: "较高",
-          description: "这是一个测试",
-          endDate: "2021-01-13",
-          sprintName: "迭代1",
-        },
-        {
-          title: "测试",
-          kind: "技术需求",
-          type: "新功能",
-          priority: "较高",
-          description: "这是一个测试",
-          endDate: "2021-01-13",
-          sprintName: "迭代1",
-        },
-        {
-          title: "测试",
-          kind: "技术需求",
-          type: "新功能",
-          priority: "较高",
-          description: "这是一个测试",
-          endDate: "2021-01-13",
-          sprintName: "迭代1",
-        },
-        {
-          title: "测试",
-          kind: "技术需求",
-          type: "新功能",
-          priority: "较高",
-          description: "这是一个测试",
-          endDate: "2021-01-13",
-          sprintName: "迭代1",
-        },
-        {
-          title: "测试",
-          kind: "技术需求",
-          type: "新功能",
-          priority: "较高",
-          description: "这是一个测试",
-          endDate: "2021-01-13",
-          sprintName: "迭代1",
+          sprintName: "第二个迭代",
         },
       ],
+      reqtList: [],
       ruleForm: {
         title: "",
         priority: "",
-        kind:"",
-        type:"",
-        endDate:"",
+        kind: "",
+        type: "",
+        endDate: "",
         description: "",
       },
       rules: {
-        title: [
-          { required: true, message: "请输入需求标题", trigger: "blur" },
-        ],
+        title: [{ required: true, message: "请输入需求标题", trigger: "blur" }],
         priority: [
           { required: true, message: "请选择优先级", trigger: "change" },
         ],
@@ -246,12 +180,32 @@ export default {
       },
     };
   },
+  created () {
+    this.show();
+  },
   methods: {
     change() {
-      this.isForm=1;
+      this.isForm = 1;
     },
-    close(){
-      this.isForm=0;
+    close() {
+      this.isForm = 0;
+    },
+    show() {
+      this.axios
+        .post("http://39.97.175.119:8801/requirement/getReqtListByPID", {
+          params: {
+            ID: this._GLOBAL.pID,
+          },
+        })
+        .then((response) => {
+          if (response.data.message == "成功") {
+            this._GLOBAL.reqtList = response.data.data.reqtList;
+            // console.log(response.data.data.reqtList[0]);
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
     },
     // tableRowClassName({row, rowIndex}) {
     //   if (rowIndex === 1) {
@@ -288,12 +242,12 @@ export default {
 .reqt_card {
   margin: 0px 24px 24px 24px;
   overflow-y: auto;
-  height: 640px;
+  height: 620px;
 }
 .form_card {
   margin: 0px 24px 24px 24px;
   width: 65%;
   height: 50%;
-  margin:auto;
+  margin: auto;
 }
 </style>
